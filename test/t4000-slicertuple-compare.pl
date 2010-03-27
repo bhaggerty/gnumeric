24c32c25ae9510bcf346661e2f003a7590ed80e6
g8cyin <yin.claudio@gmail.com>
2010-03-27 17:32:43 -0400

    Testing framework - hooks set up for compare_to

---
+++ test/t4000-slicertuple-compare.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl -w
+# -----------------------------------------------------------------------------
+
+use strict;
+use lib ($0 =~ m|^(.*/)| ? $1 : ".");
+use GnumericTest;
+
+my $expected;
+{ local $/; $expected = <DATA>; }
+
+&message ("Checking function go_data_slicer_tuple_compare_to.");
+&sstest ("test_go_data_slicer_tuple_compare_to", $expected);
+
+__DATA__
+-----------------------------------------------------------------------------
+Start: test_go_data_slicer_tuple_compare_to
+-----------------------------------------------------------------------------
+
+pass haha
+End: test_go_data_slicer_tuple_compare_to
